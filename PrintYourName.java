f2e3e4abf2b3249e98fdfc15239eccc67d7436de
Hussain <hussainms704@gmail.com>
2026-01-04 21:18:01 +0530

    Day 1: Java basics, input and swap

---
+++ PrintYourName.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class PrintYourName{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Enter Your Name Is :");
+        String name = scan.nextLine();
+        System.out.println("Your name is :" + name);
+        
+       
+    }
+}
+
+
